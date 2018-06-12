8230180510bf2328024997c71ee0349282c44e8b
icyvexen <ajay.brown@ndsu.edu>
2018-06-12 18:41:42 -0500

    Added Basic state scripting

---
+++ Byte-le-Royale-2018-Visualizer/Assets/StateScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateScript : MonoBehaviour {
+    List<GameObject> ShipList;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		foreach(GameObject x in ShipList)
+        {
+            Rigidbody y = x.GetComponent<Rigidbody>();
+            y.MovePosition(new Vector3(y.position.x, y.position.y, y.position.z + 10));
+        }
+	}
+}
